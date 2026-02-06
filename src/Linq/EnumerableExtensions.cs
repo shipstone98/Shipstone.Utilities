5f563de3450f81ec29143b40672cc116e9ae27e7
Christopher Shipstone <christopher.shipstone98@gmail.com>
2026-02-06 11:35:47 +0000

    Added SelectManyAsync method

--- src/Linq/EnumerableExtensions.cs
+++ src/Linq/EnumerableExtensions.cs
@@ -38,8 +38,8 @@ public static class EnumerableExtensions
         );
     }
 
-    private static async IAsyncEnumerable<TResult> SelectAsyncCore<TSource, TResult>(
-        IEnumerable<TSource> source,
+    internal static async IAsyncEnumerable<TResult> SelectAsyncCore<TSource, TResult>(
+        this IEnumerable<TSource> source,
         Func<TSource, int, CancellationToken, Task<TResult>> selector,
         [EnumeratorCancellation] CancellationToken cancellationToken
     )
@@ -54,6 +54,59 @@ public static class EnumerableExtensions
             }
 
             yield return await selector(item, index, cancellationToken);
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously projects each element of a sequence to an <see cref="IEnumerable{T}" />, and flattens the resulting sequences into one sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of elements of the source collection.</typeparam>
+    /// <typeparam name="TResult">The type of the elements of the sequence returned by the transform function.</typeparam>
+    /// <param name="source">A sequence of values to invoke a transform function on.</param>
+    /// <param name="selector">An asynchronous transform function to apply to each source element; the second parameter of the function represents the index of the source element.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns>An <see cref="IAsyncEnumerable{T}" /> whose elements are the result of invoking <c><paramref name="selector" /></c> on each element of <c><paramref name="source" /></c>.</returns>
+    /// <exception cref="ArgumentNullException"><c><paramref name="source" /></c> is <c>null</c> -or- <c><paramref name="selector" /></c> is <c>null</c>.</exception>
+    /// <exception cref="OperationCanceledException">The cancellation token was canceled.</exception>
+    public static IAsyncEnumerable<TResult> SelectManyAsync<TSource, TResult>(
+        this IEnumerable<TSource> source,
+        Func<TSource, int, CancellationToken, IAsyncEnumerable<TResult>> selector,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return EnumerableExtensions.SelectManyAsyncCore(
+            source,
+            selector,
+            cancellationToken
+        );
+    }
+
+    private static async IAsyncEnumerable<TResult> SelectManyAsyncCore<TSource, TResult>(
+        IEnumerable<TSource> source,
+        Func<TSource, int, CancellationToken, IAsyncEnumerable<TResult>> selector,
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        int index = -1;
+
+        foreach (TSource item in source)
+        {
+            checked
+            {
+                ++ index;
+            }
+
+            await foreach (TResult resultItem in selector(
+                item,
+                index,
+                cancellationToken
+            ))
+            {
+                yield return resultItem;
+            }
         }
     }
 }
--- tests/Linq/EnumerableExtensionsTest.cs
+++ tests/Linq/EnumerableExtensionsTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -100,4 +101,108 @@ public sealed class EnumerableExtensionsTest
         }
     }
 #endregion
+
+#region SelectManyAsync method
+    [Fact]
+    public void TestSelectManyAsync_Invalid_SelectorNull()
+    {
+        // Arrange
+        IEnumerable<Object> source = Array.Empty<Object>();
+
+        // Act
+        ArgumentException ex =
+            Assert.Throws<ArgumentNullException>(() =>
+                EnumerableExtensions.SelectManyAsync<Object, Object>(
+                    source,
+                    null!
+                ));
+
+        // Assert
+        Assert.Equal("selector", ex.ParamName);
+    }
+
+    [Fact]
+    public void TestSelectManyAsync_Invalid_SourceNull()
+    {
+        // Act
+        ArgumentException ex =
+            Assert.Throws<ArgumentNullException>(() =>
+                EnumerableExtensions.SelectManyAsync<Object, Object>(
+                    null!,
+                    (_, _, _) => throw new NotImplementedException()
+                ));
+
+        // Assert
+        Assert.Equal("source", ex.ParamName);
+    }
+
+    [Fact]
+    public async Task TestSelectManyAsync_Valid_Empty()
+    {
+        // Arrange
+        IEnumerable<Object> source = Array.Empty<Object>();
+
+        // Act
+        IAsyncEnumerable<Object> resultActual =
+            EnumerableExtensions.SelectManyAsync<Object, Object>(
+                source,
+                (_, _, _) => throw new NotImplementedException()
+            );
+
+        // Assert
+        await using IAsyncEnumerator<Object> enumerator =
+            resultActual.GetAsyncEnumerator();
+
+        Assert.False(await enumerator.MoveNextAsync());
+    }
+
+    [Fact]
+    public async Task TestSelectManyAsync_Valid_NotEmpty()
+    {
+        // Arrange
+        IEnumerable<String> source = new String[] { "123", "456", "789" };
+        ISet<int> indicesActual = new SortedSet<int>();
+
+        // Act
+        IAsyncEnumerable<char> resultActual =
+            EnumerableExtensions.SelectManyAsync(
+                source,
+                (s, i, _) =>
+                {
+                    indicesActual.Add(i);
+                    return EnumerableExtensionsTest.SelectAsync(s);
+                }
+            );
+
+        // Assert
+        IEnumerable<char> resultExpected =
+            new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        using IEnumerator<char> expectedEnumerator =
+            resultExpected.GetEnumerator();
+
+        await using IAsyncEnumerator<char> actualEnumerator =
+            resultActual.GetAsyncEnumerator();
+
+        while (expectedEnumerator.MoveNext())
+        {
+            Assert.True(await actualEnumerator.MoveNextAsync());
+            Assert.Equal(expectedEnumerator.Current, actualEnumerator.Current);
+        }
+
+        Assert.False(await actualEnumerator.MoveNextAsync());
+        IEnumerable<int> indicesExpected = new int[] { 0, 1, 2 };
+        Assert.True(indicesExpected.SequenceEqual(indicesActual));
+    }
+#endregion
+
+    private static async IAsyncEnumerable<char> SelectAsync(
+        String source
+    )
+    {
+        foreach (char c in source)
+        {
+            yield return c;
+        }
+    }
 }
