654a90965306878554a6345c0252954858b693a7
Christopher Shipstone <christopher.shipstone98@gmail.com>
2026-02-12 15:54:44 +0000

    Fixed documentation

--- src/Linq/PaginatedListExtensions.cs
+++ src/Linq/PaginatedListExtensions.cs
@@ -16,9 +16,9 @@ public static class PaginatedListExtensions
     /// <summary>
     /// Asynchronously projects each element of a paginated list into a new form by incorporating the element's index.
     /// </summary>
-    /// <typeparam name="TSource">The type of elements of the source collection.</typeparam>
+    /// <typeparam name="TSource">The type of elements of the source paginated list.</typeparam>
     /// <typeparam name="TResult">The type of the value returned by the transform function.</typeparam>
-    /// <param name="source">A sequence of values to invoke a transform function on.</param>
+    /// <param name="source">A paginated list of values to invoke a transform function on.</param>
     /// <param name="selector">An asynchronous transform function to apply to each source element; the second parameter of the function represents the index of the source element.</param>
     /// <returns>An <see cref="IReadOnlyPaginatedList{T}" /> whose elements are the result of invoking <c><paramref name="selector" /></c> on each element of <c><paramref name="source" /></c>.</returns>
     /// <exception cref="ArgumentNullException"><c><paramref name="source" /></c> is <c>null</c> -or- <c><paramref name="selector" /></c> is <c>null</c>.</exception>
