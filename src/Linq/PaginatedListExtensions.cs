72ce5987bdadd198db934f277b914e2ab3bd8cd0
Christopher Shipstone <christopher.shipstone98@gmail.com>
2026-02-12 15:54:54 +0000

    Added SelectAsync method

--- src/Linq/PaginatedListExtensions.cs
+++ src/Linq/PaginatedListExtensions.cs
@@ -42,4 +42,51 @@ public static class PaginatedListExtensions
             source.PageCount
         );
     }
+
+    /// <summary>
+    /// Asynchronously projects each element of a paginated list into a new form by incorporating the element's index.
+    /// </summary>
+    /// <typeparam name="TSource">The type of elements of the source paginated list.</typeparam>
+    /// <typeparam name="TResult">The type of the value returned by the transform function.</typeparam>
+    /// <param name="source">A paginated list of values to invoke a transform function on.</param>
+    /// <param name="selector">An asynchronous transform function to apply to each source element; the second parameter of the function represents the index of the source element.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns>An <see cref="IReadOnlyPaginatedList{T}" /> whose elements are the result of invoking <c><paramref name="selector" /></c> on each element of <c><paramref name="source" /></c>.</returns>
+    /// <exception cref="ArgumentNullException"><c><paramref name="source" /></c> is <c>null</c> -or- <c><paramref name="selector" /></c> is <c>null</c>.</exception>
+    /// <exception cref="OperationCanceledException">The cancellation token was canceled.</exception>
+    public static Task<IReadOnlyPaginatedList<TResult>> SelectAsync<TSource, TResult>(
+        this IReadOnlyPaginatedList<TSource> source,
+        Func<TSource, int, CancellationToken, Task<TResult>> selector,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return PaginatedListExtensions.SelectAsyncCore(
+            source,
+            selector,
+            cancellationToken
+        );
+    }
+
+    private static async Task<IReadOnlyPaginatedList<TResult>> SelectAsyncCore<TSource, TResult>(
+        IReadOnlyPaginatedList<TSource> source,
+        Func<TSource, int, CancellationToken, Task<TResult>> selector,
+        CancellationToken cancellationToken
+    )
+    {
+        IList<TResult> list =
+            await source
+                .SelectAsyncCore(selector, cancellationToken)
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+        return new ReadOnlyPaginatedCollection<TResult>(
+            list,
+            source.TotalCount,
+            source.PageIndex,
+            source.PageCount
+        );
+    }
 }
--- tests/Linq/PaginatedListExtensionsTest.cs
+++ tests/Linq/PaginatedListExtensionsTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 using Shipstone.Utilities.Collections;
@@ -97,6 +98,111 @@ public sealed class PaginatedListExtensionsTest
                     indices.Add(i);
                     return n.ToString();
                 });
+
+        // Assert
+        IReadOnlyList<String> list =
+            sourceCollection
+                .Select(n => n.ToString())
+                .ToList();
+
+        result.AssertEqual(list, TOTAL_COUNT, PAGE_INDEX, PAGE_COUNT);
+        Assert.Equal(COUNT, indices.Count);
+        int index = 0;
+
+        foreach (int indicesIndex in indices)
+        {
+            Assert.Equal(index ++, indicesIndex);
+        }
+    }
+#endregion
+
+#region SelectAsync method
+    [Fact]
+    public async Task TestSelectAsync_Invalid_SelectorNull()
+    {
+        // Arrange
+        IReadOnlyPaginatedList<Object> source =
+            new MockReadOnlyPaginatedList<Object>();
+
+        // Act
+        ArgumentException ex =
+            await Assert.ThrowsAsync<ArgumentNullException>(() =>
+                PaginatedListExtensions.SelectAsync<Object, Object>(
+                    source,
+                    null!
+                ));
+
+        // Assert
+        Assert.Equal("selector", ex.ParamName);
+    }
+
+    [Fact]
+    public async Task TestSelectAsync_Invalid_SourceNull()
+    {
+        // Act
+        ArgumentException ex =
+            await Assert.ThrowsAsync<ArgumentNullException>(() =>
+                PaginatedListExtensions.SelectAsync<Object, Object>(
+                    null!,
+                    (_, _, _) => throw new NotImplementedException()
+                ));
+
+        // Assert
+        Assert.Equal("source", ex.ParamName);
+    }
+
+    [Fact]
+    public async Task TestSelectAsync_Valid_Empty()
+    {
+        // Arrange
+        IEnumerable<Object> sourceCollection = Array.Empty<Object>();
+        MockReadOnlyPaginatedList<Object> source = new();
+        source._getEnumeratorFunc = sourceCollection.GetEnumerator;
+        source._totalCountFunc = () => 0;
+        source._pageIndexFunc = () => 0;
+        source._pageCountFunc = () => 1;
+
+        // Act
+        IReadOnlyPaginatedList<Object> result =
+            await PaginatedListExtensions.SelectAsync<Object, Object>(
+                source,
+                (_, _, _) => throw new NotImplementedException()
+            );
+
+        // Assert
+        result.AssertEmpty();
+    }
+
+    [Fact]
+    public async Task TestSelectAsync_Valid_NotEmpty()
+    {
+        // Arrange
+        const int COUNT = 5;
+
+        IEnumerable<int> sourceCollection =
+            new int[COUNT] { 11, 12, 13, 14, 15 };
+
+        const int TOTAL_COUNT = 113;
+        const int PAGE_INDEX = 17;
+        const int PAGE_COUNT = 23;
+        MockReadOnlyPaginatedList<int> source = new();
+        source._getEnumeratorFunc = sourceCollection.GetEnumerator;
+        source._totalCountFunc = () => TOTAL_COUNT;
+        source._pageIndexFunc = () => PAGE_INDEX;
+        source._pageCountFunc = () => PAGE_COUNT;
+        ISet<int> indices = new SortedSet<int>();
+
+        // Act
+        IReadOnlyPaginatedList<String> result =
+            await PaginatedListExtensions.SelectAsync(
+                source,
+                (n, i, _) =>
+                {
+                    indices.Add(i);
+                    String s = n.ToString();
+                    return Task.FromResult(s);
+                }
+            );
 
         // Assert
         IReadOnlyList<String> list =
